71943aab42bdbdfc9b34331af84697728860407e
mikinty <youmichaelc@gmail.com>
2020-07-30 23:29:30 -0400

    Get weekly and daily volatilities. Trying to calculate chance of profit with Gaussian

--- src/analysis/vol.js
+++ src/analysis/vol.js
@@ -14,6 +14,9 @@ import { LAMBDA_EWMA } from './CONST_ANALYSIS.js';
 
 /**
  * Calculates the variance based on simple avg, EWMA
+ *
+ * We convert everything to \sigma at the end instead
+ * of leaving it squared
  * 
  * @param { float[] } data Price data
  * @param { int } window_size 
@@ -39,19 +42,28 @@ export function vol_AVG (data, lambda=LAMBDA_EWMA) {
     for (let j = 0; j <= i; j++) {
       curr_sum += returns[j];
     }
-    vol_sa.push(curr_sum/(i+1));
+
+    vol_sa.push(
+      100*Math.sqrt(
+        curr_sum/(i+1)
+      )
+    );
   }
 
   // EWMA
-  let vol_ewma = [returns[0]];
+  let vol_ewma = [
+    returns[0]
+  ];
   for (let i = 1; i < returns.length; i++) {
     vol_ewma.push(
       (1-lambda)*returns[i] + lambda*vol_ewma[i-1]
     );
   }
 
+  let sqrt_vol_ewma = vol_ewma.map(e => 100*Math.sqrt(e));
+
   return {
     vol_sa: vol_sa,
-    vol_ewma: vol_ewma
+    vol_ewma: sqrt_vol_ewma
   };
 }
--- src/index.js
+++ src/index.js
@@ -50,6 +50,7 @@ async function init () {
   // Stonk we are analyzing
   let STONK_TICKER = 'BYND';
 
+  title.innerHTML = 'getting quotes...';
   /* Current snapshot of stock */
   let curr_price = await YF.quote({
     symbol: STONK_TICKER
@@ -66,17 +67,24 @@ async function init () {
   curr_date.setFullYear(curr_date.getFullYear() - 1);
   let past_date_string = `${curr_date.getFullYear()}-${curr_date.getMonth()+1}-${curr_date.getDate()}`;
 
-  let quotes = await YF.historical({
+  let quotes_d = await YF.historical({
     symbol: STONK_TICKER,
     from: past_date_string,
     to: curr_date_string,
     period: 'd'
   });
 
+  let quotes_w = await YF.historical({
+    symbol: STONK_TICKER,
+    from: past_date_string,
+    to: curr_date_string,
+    period: 'w'
+  });
+
   // set the dimensions and margins of the graph
   var margin = {top: 20, right: 20, bottom: 50, left: 70};
-  var width = 460- margin.left - margin.right;
-  var height = 500 - margin.top - margin.bottom;
+  var width = 460 - margin.left - margin.right;
+  var height = 450 - margin.top - margin.bottom;
 
   // set the ranges
   var x = d3.scaleTime().range([0, width]);
@@ -94,15 +102,13 @@ async function init () {
       'translate(' + margin.left + ',' + margin.top + ')'
     );
 
-  let data = quotes;
-
   // Scale the range of the data
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain([0, d3.max(data, function(d) { return d.close; })]);
+  x.domain(d3.extent(quotes_d, function(d) { return d.date; }));
+  y.domain([0, d3.max(quotes_d, function(d) { return d.close; })]);
 
   // Add the valueline path.
   svg.append('path')
-    .data([data])
+    .data([quotes_d])
     .attr('fill', 'none')
     .attr('stroke', 'steelblue')
     .attr('stroke-width', 1.5)
@@ -114,7 +120,13 @@ async function init () {
   // Add the x Axis
   svg.append('g')
     .attr('transform', 'translate(0,' + height + ')')
-    .call(d3.axisBottom(x));
+    .call(d3.axisBottom(x))
+    .selectAll('text')
+    .attr('y', 0)
+    .attr('x', 9)
+    .attr('dy', '.35em')
+    .attr('transform', 'rotate(90)')
+    .style('text-anchor', 'start');
 
   // Add the y Axis
   svg.append('g')
@@ -122,13 +134,21 @@ async function init () {
 
   // Concern: when we do things like this we lose the date that is associated
   // with each price
-  let prices = quotes.map((e) => e.close);
-  let vol_res = {
-    '2w' : vol_AVG(prices.slice(0, 10)),
-    '1m' : vol_AVG(prices.slice(0, 20)),
-    '3m' : vol_AVG(prices.slice(0, 60)),
-    '6m' : vol_AVG(prices.slice(0, 120)),
-    '1y' : vol_AVG(prices)
+  let prices_d = quotes_d.map((e) => e.close);
+  let prices_w = quotes_w.map((e) => e.close);
+
+  let vol_res_d = {
+    '2w' : vol_AVG(prices_d.slice(0, 10)),
+    '1m' : vol_AVG(prices_d.slice(0, 20)),
+    '3m' : vol_AVG(prices_d.slice(0, 60)),
+    '6m' : vol_AVG(prices_d.slice(0, 120)),
+    '1y' : vol_AVG(prices_d)
+  };
+
+  let vol_res_w = {
+    '3m' : vol_AVG(prices_w.slice(0, 12)),
+    '6m' : vol_AVG(prices_w.slice(0, 24)),
+    '1y' : vol_AVG(prices_w)
   };
 
   // Plot volatility
@@ -136,12 +156,12 @@ async function init () {
   // have the volatility for the first day
   let data_vol = [];
 
-  for (let i = 1; i < quotes.length; i++) {
+  for (let i = 1; i < quotes_d.length; i++) {
     data_vol.push(
       {
-        date: quotes[i]['date'],
-        vol_sa: vol_res['1y']['vol_sa'][i-1],
-        vol_ewma: vol_res['1y']['vol_ewma'][i-1]
+        date: quotes_d[i]['date'],
+        vol_sa: vol_res_d['1y']['vol_sa'][i-1],
+        vol_ewma: vol_res_d['1y']['vol_ewma'][i-1]
       }
     );
   }
@@ -153,7 +173,7 @@ async function init () {
   var x_2 = d3.scaleTime().range([0, width]);
   var y_2 = d3.scaleLinear().range([height, 0]);
 
-  // append the svg obgect to the body of the page
+  // append the svg object to the body of the page
   // appends a 'group' element to 'svg'
   // moves the 'group' element to the top left margin
   var svg_2 = d3.select('#chart_vol').append('svg')
@@ -169,7 +189,18 @@ async function init () {
   x_2.domain(d3.extent(data_vol, function(d) { return d.date; }));
   y_2.domain([0, d3.max(data_vol, function(d) { return d.vol_ewma; })]);
 
-  // Add the valueline path.
+  // Plot the price under the volatility
+  svg_2.append('path')
+    .data([quotes_d])
+    .attr('fill', 'none')
+    .attr('stroke', CONST_STYLE.GREEN_BYND)
+    .attr('stroke-width', 1.5)
+    .attr('d', d3.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.close); })
+    );
+  
+  // Plot the volatility
   svg_2.append('path')
     .data([data_vol])
     .attr('fill', 'none')
@@ -180,21 +211,16 @@ async function init () {
       .y(function(d) { return y_2(d.vol_ewma); })
     );
 
-
-  svg_2.append('path')
-    .data([data])
-    .attr('fill', 'none')
-    .attr('stroke', CONST_STYLE.GREEN_BYND)
-    .attr('stroke-width', 1.5)
-    .attr('d', d3.line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.close); })
-    );
-
   // Add the x Axis
   svg_2.append('g')
     .attr('transform', 'translate(0,' + height + ')')
-    .call(d3.axisBottom(x_2));
+    .call(d3.axisBottom(x_2))
+    .selectAll('text')
+    .attr('y', 0)
+    .attr('x', 9)
+    .attr('dy', '.35em')
+    .attr('transform', 'rotate(90)')
+    .style('text-anchor', 'start');
 
   // Add the y Axis
   svg_2.append('g')
@@ -232,18 +258,55 @@ async function init () {
     .attr('alignment-baseline', 'middle');
 
   let output_vol = '';
-  for (const [key, value] of Object.entries(vol_res)) {
+
+  // First do the weekly
+  output_vol += 
+  `
+  <th colspan="3">Granularity: 1w</th>
+  <tr>
+    <th>Sample timeline</th>
+    <th>SA</th>
+    <th>EWMA</th>
+  </tr>
+  `;
+  for (const [key, value] of Object.entries(vol_res_w)) {
     output_vol +=
     `<tr>
-      <th>
+      <td>
       ${key}
-      </th>
-      <th>
-      ${(100*Math.sqrt(value.vol_sa[value.vol_sa.length-1])).toFixed(3)}%
-      </th>
-      <th>
-      ${(100*Math.sqrt(value.vol_ewma[value.vol_ewma.length-1])).toFixed(3)}%
-      </th>
+      </td>
+      <td>
+      ${(value.vol_sa[value.vol_sa.length-1]).toFixed(3)}%
+      </td>
+      <td>
+      ${(value.vol_ewma[value.vol_ewma.length-1]).toFixed(3)}%
+      </td>
+    </tr>
+    `;
+  }
+
+  // Do the daily volatility
+  output_vol += 
+  `
+  <th colspan="3">Granularity: 1d</th>
+  <tr>
+    <th>Sample timeline</th>
+    <th>SA</th>
+    <th>EWMA</th>
+  </tr>
+  `;
+  for (const [key, value] of Object.entries(vol_res_d)) {
+    output_vol +=
+    `<tr>
+      <td>
+      ${key}
+      </td>
+      <td>
+      ${(value.vol_sa[value.vol_sa.length-1]).toFixed(3)}%
+      </td>
+      <td>
+      ${(value.vol_ewma[value.vol_ewma.length-1]).toFixed(3)}%
+      </td>
     </tr>
     `;
   }
@@ -255,25 +318,48 @@ async function init () {
 
   // let calls = options['optionChain']['result'][0]['options'][0]['calls'];
   let puts  = options['optionChain']['result'][0]['options'][0]['puts'];
-  
-  console.log(puts[0]);
+  /*
+   * Sample output:
+   *
+   * ask: 0.01
+   * bid: 0
+   * change: 0
+   * contractSize: "REGULAR"
+   * contractSymbol: "BYND200731P00065000"
+   * currency: "USD"
+   * expiration: 1596153600
+   * impliedVolatility: 3.93750015625
+   * inTheMoney: false
+   * lastPrice: 0.01
+   * lastTradeDate: 1595943521
+   * openInterest: 154
+   * percentChange: 0
+   * strike: 65
+   * volume: 2
+   */
+
+  const one_day = (24 * 60 * 60 * 1000); // hours*minutes*seconds*milliseconds
+  const diff_days = Math.ceil(Math.abs((new Date()) - (new Date(1000*puts[0]['expiration']))) / one_day);
 
   let output_puts = '';
   for (let i = 0; i < puts.length; i++) {
     output_puts +=
     `<tr>
-      <th>
-      ${puts[i]['contractSymbol']}
-      </th>
-      <th>
+      <td>
       ${Number(puts[i]['strike']).toFixed(2)}
-      </th>
-      <th>
+      </td>
+      <td>
       ${Number(puts[i]['lastPrice']).toFixed(2)}
-      </th>
-      <th>
-      ${Number(100*puts[i]['impliedVolatility']).toFixed(2)}
-      </th>
+      </td>
+      <td>
+      ${Number(100*puts[i]['impliedVolatility']).toFixed(2)}%
+      </td>
+      <td>
+      ${vol_res_d['2w'].vol_ewma[vol_res_d['2w'].vol_ewma.length - 1]*diff_days}
+      </td>
+      <td>
+      ${vol_res_w['3m'].vol_ewma[vol_res_w['3m'].vol_ewma.length - 1]*diff_days/5}
+      </td>
     </tr>
     `;
   }
@@ -282,10 +368,11 @@ async function init () {
   `
   <table style="width:100%">
   <tr>
-    <th>Contract Name</th>
-    <th>Strike Price</th>
+    <th>Strike</th>
     <th>Last Price</th>
     <th>IV</th>
+    <th>COP_d</th>
+    <th>COP_w</th>
   </tr>
   ${output_puts}
   </table>
@@ -293,13 +380,7 @@ async function init () {
 
   vol_analysis.innerHTML = 
   `
-  \\[\\sigma\\]
   <table style="width:100%">
-  <tr>
-    <th>Time period</th>
-    <th>SA</th>
-    <th>EWMA</th>
-  </tr>
   ${output_vol}
   </table>
   `;
